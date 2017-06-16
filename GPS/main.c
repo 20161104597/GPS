9feaaee5da2d72848cffd50d3570020a3c9d241b
20161104597 <a20161104597@Mac15.local>
2017-06-16 18:08:41 +0800

    GPS

--- GPS/main.c
+++ GPS/main.c
@@ -7,9 +7,18 @@
 //
 
 #include <stdio.h>
+int main()
+{
+    int i;
+    FILE *fp1,*fp2;
+char s[50];
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+fp1=fopen("//Users//a20161104597//Desktop//GPS//GPS170510.log","w");
+fp2=fopen("//Users//a20161104597//Desktop//GPS//GPS170510.log//out.txt","r");
+    for(i=0;i<50;i++)
+    {
+        fscanf(fp1,"%d",&s[i]);
+    }
+    fclose(fp1);
     return 0;
 }
