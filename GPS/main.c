8a1528aba3442ffcf96cbecbfcd911a08aa06f37
20161104597 <a20161104597@Mac15.local>
2017-06-22 11:52:40 +0800

    gps
    .

--- GPS/main.c
+++ GPS/main.c
@@ -13,24 +13,24 @@ int main()
     FILE *fp1;
     char str1[65];
     char str2[65];
-    char lat1[9];
-    char lat2[10];
+    char lat[9];
+    char lon[10];
     int i,m;
     fp1=fopen("//Users//a20161104597//Desktop//GPS//GPS170510.log","r");
     fscanf(fp1,"%s%s",str1,str2);
     printf("结果: %s\n%s\n",str1,str2);
     for(i=0;i<8;i++)
     {
-    lat1[i]=str1[i+16];
-        lat1[8]='\0';
+    lat[i]=str1[i+16];
+        lat[8]='\0';
     }
     for(m=0;m<9;m++)
     {
-        lat2[m]=str2[m+25];
-        lat2[9]='\n';
+        lon[m]=str2[m+25];
+        lon[9]='\0';
     }
-    printf("纬度:%s\n",lat1);
-    printf("经度:%s\n",lat2);
+    printf("纬度:%s\n",lat);
+    printf("经度:%s\n",lon);
     fclose(fp1);
     return 0;
 }
